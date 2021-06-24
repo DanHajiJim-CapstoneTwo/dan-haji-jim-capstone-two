751bfc1874ddb74797a368821c56fd2c7896926e
dliebman100 <dliebman100@gmail.com>
2021-06-24 10:18:13 -0400

    Dog api (#2)
    * react router config
    
    * react router added cart, header, photos
    
    * added react router
    
    * fix: react router
    
    * fix: updated react router
    
    * removed, restart, fixed
    
    * modified className
    
    * lowered limit=5 test
    
    * fixed API issue
    
    * added new images in CSS
    
    * added styling to Header and Pokemon card
    
    * fixed: added styles and box shadow
    
    * v.1.0.1 up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// DEPENDENCIES
+import React, { Component } from "react";
+// CHILD COMPONENTS
+import Header from "./Components/Header/Header";
+import PokeProducts from "./Components/PokeProducts/PokeProducts";
+import Card from "./Components/Card/Card";
+// STYLING
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// CLASS COMPONENT
+class App extends Component {
+  constructor() {
+    super();
+    // STATE
+    this.state = {
+      owner: "",
+    };
+  }
+  // RENDER APP
+  render() {
+    const { owner } = this.state;
+    return (
+      <div className="App">
+        <Header name={owner} />
+        <PokeProducts />
+        <Card />
+      </div>
+    );
+  }
 }
-
+//change return from SearchBar "capstone"
+//handleChange = (event) => {
+//   this.setState({value: event.target.value});
+// }
 export default App;
